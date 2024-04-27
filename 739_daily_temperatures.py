4764b2c31a3706f3ccacd4e58a7fdcfbb5a864a0
comdori <leeseunghoon88@gmail.com>
2024-04-27 10:06:51 -0400

    Create 739_daily_temperatures.py

---
+++ 739_daily_temperatures.py
@@ -0,0 +1,12 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        answer = [0] * len(temperatures)
+        stack = []
+
+        for i, temp in enumerate(temperatures):
+            while stack and temperatures[stack[-1]] < temp:
+                idx = stack.pop()
+                answer[idx] = i - idx
+            stack.append(i)
+
+        return answer
