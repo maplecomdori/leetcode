5f4db3180938e3d5f9c9b09bb32cc3e662b975c8
comdori <leeseunghoon88@gmail.com>
2024-10-20 14:11:08 -0400

    Update 921_minimum_add_to_make_parentheses_valid.py

--- 921_minimum_add_to_make_parentheses_valid.py
+++ 921_minimum_add_to_make_parentheses_valid.py
@@ -1,5 +1,23 @@
 class Solution:
     def minAddToMakeValid(self, s: str) -> int:
+        # count solution
+
+        # count parentheses that were not closed, left over ( or )
+        open_count = 0
+        close_count = 0
+
+        for elem in s:
+            if elem == '(':
+                open_count += 1
+            elif elem == ')':
+                close_count += 1
+                if open_count > 0:
+                    open_count -= 1
+                    close_count -= 1
+
+        return open_count + close_count
+
+        # stack solution
         stack = []
 
         for elem in s:
