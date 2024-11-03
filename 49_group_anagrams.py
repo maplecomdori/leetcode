118bf5c779db993554d36179541a590ecb26e5a6
comdori <leeseunghoon88@gmail.com>
2024-11-02 21:51:09 -0400

    Update 49_group_anagrams.py

--- 49_group_anagrams.py
+++ 49_group_anagrams.py
@@ -1,12 +1,11 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        dic = defaultdict(list) # {key: [str]}, where key = 030230 each digit represents a occurence of a char
+        dic = defaultdict(list)  # {key: [str]}
 
         for s in strs:
             count = [0] * 26
             for char in s:
                 count[ord(char) - ord('a')] += 1
-            # key = "-".join([str(n) for n in count])
             dic[tuple(count)].append(s)
 
-        return dic.values()
+        return list(dic.values())
