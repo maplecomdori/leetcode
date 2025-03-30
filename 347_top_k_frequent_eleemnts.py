3819fb2c38d46eb2ab3edcd1723c5f6584248a92
comdori <leeseunghoon88@gmail.com>
2025-03-30 10:15:26 -0400

    Update 347_top_k_frequent_eleemnts.py

--- 347_top_k_frequent_eleemnts.py
+++ 347_top_k_frequent_eleemnts.py
@@ -1,5 +1,22 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # heap solution
+        # count frequencies of each number
+        counter = {} # {number: count}
+        for n in nums:
+            counter[n] = counter.get(n, 0) + 1
+
+        # put them in min_heap to pop numbers with smallest frequencies
+        min_heap = []
+        for number, count in counter.items():
+            heapq.heappush(min_heap, (count, number))
+            if len(min_heap) > k:
+                heapq.heappop(min_heap)
+
+        # pop k elements from the heap
+        res = [n for count, n in min_heap]
+        return res
+
         # bucket sort
         lst = [[] for i in range(len(nums) + 1)]  # lst[j] contains elements that occur j times
         counter = Counter(nums)
