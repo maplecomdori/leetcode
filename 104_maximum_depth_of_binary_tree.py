406f1a67af46150d1ea37acc3f341bc7c8924424
comdori <leeseunghoon88@gmail.com>
2025-06-14 15:10:33 +0900

    Update 104_maximum_depth_of_binary_tree.py

--- 104_maximum_depth_of_binary_tree.py
+++ 104_maximum_depth_of_binary_tree.py
@@ -13,3 +13,21 @@ class Solution:
             return 1 + max(rec(node.left), rec(node.right))
 
         return rec(root)
+
+        # iterative solution
+        if not root:
+            return 0
+        queue = deque()
+        depth = 0
+        queue.append(root)
+
+        while queue:
+            for _ in range(len(queue)):
+                pop = queue.popleft()
+                if pop.left:
+                    queue.append(pop.left)
+                if pop.right:
+                    queue.append(pop.right)
+            depth += 1
+
+        return depth
