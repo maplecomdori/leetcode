ca25eb30e413a6ebbfa194c27bc09e95573b35eb
comdori <leeseunghoon88@gmail.com>
2025-07-02 09:52:51 +0900

    Update 153_find_minimum_in_rotated_sorted_array.py

--- 153_find_minimum_in_rotated_sorted_array.py
+++ 153_find_minimum_in_rotated_sorted_array.py
@@ -1,37 +1,55 @@
 class Solution:
     def findMin(self, nums: List[int]) -> int:
+
         left = 0
         right = len(nums) - 1
+        min_val = 10 ** 5
 
         while left <= right:
             mid = (left + right) // 2
 
-            if nums[left] <= nums[mid] <= nums[right]:
-                return nums[left]
-            elif mid > 0 and nums[mid - 1] > nums[mid]:
-                return nums[mid]
-            elif nums[mid] > nums[right]:  # go right
-                left = mid + 1
-            else:  # go left
-                right = mid - 1
+            min_val = min(min_val, nums[mid])
 
+            if nums[mid] < nums[right]:
+                # right side is ordered => nums[mid] is minimum from mid to right
+                # and search left further to find anything less than nums[mid]
+                right = mid - 1
+            else:
+                left = mid + 1
+
+        return min_val
 
 """
 1   2   3   4   5
-l       m       r   nums[l] <= nums[m] < nums[r] => return nums[l]
+l       m       r   right side is ordered => minimum must be on the left side
+lm  r
+l
 
-5   1   2   3   4   else go right
+2   3   4   5   1
+l       m       r   right side is not ordered => minimum must be to the right of nums[mid]
+            lm  r
+                lr  
+
+3   4   5   1   2
 l       m       r
+            lm  r   mid > right => search right
 
 4   5   1   2   3
-l       m       r   nums[m-1] > nums[m] => return nums[m]
+l       m       r   mid < right => search left
+lm  r               mid < right => search left
 
-3   4   5   1   2   left is sorted, right is not sorted => go right 
-l       m       r   nums[mid] > nums[right] => go right
-            l   r
+5   1   2   3   4
+l       m       r   mid < right => search left
 
-2   3   4   5   1   nums[mid] > nums[right] => go right
-l       m       r
-            l   r
+0
+lr
 
+1   2   3   4
+l   m       r
+lr
+
+2   3   4   1
+l   m       r
+        lm  r
+            lr
 """
