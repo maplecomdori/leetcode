a94211ab8d1f830ddc30df23263688d090d769ff
comdori <leeseunghoon88@gmail.com>
2025-07-05 10:10:42 +0900

    Update 90_subsets_2.py

--- 90_subsets_2.py
+++ 90_subsets_2.py
@@ -3,53 +3,49 @@ class Solution:
         res = []
         builder = []
         nums.sort()
-        seen = set() # { tuple }
-
         def rec(i):
+            res.append(builder[:])
             if i == len(nums):
-                tup = tuple(builder[:])
-                if tup not in seen:
-                    res.append(builder[:])
-                    seen.add(tup)
                 return
-
-            # include
-                builder.append(nums[i])
-                rec(i + 1)
+            for j in range(i, len(nums)):
+                if j > i and nums[j] == nums[j-1]:
+                    continue
+                builder.append(nums[j])
+                rec(j + 1)
                 builder.pop()
 
-            # skip
-            rec(i + 1)
-
-
         rec(0)
+
         return res
 
-
-
-
-
-
 """
-0
-[0]
-
-
 1   2   2
-[]  [1] [1,2] [1,2,2], [2], [2,2]
-
+[]
 1
 1   2
-1   2   2   add
-1   2   X   add
-1   X   2   duplicate
-1   X   X   add
+1   2   2
+1       2   duplicate
 
-X
-X   2
-X   2   2   add
-X   2   X   Add
-X   X   2   duplicate
+    2
+    2   2
+        2   duplicate => skip a number if it is the same as the prev
+
+
+1   1   2   2
+[]
+1
+1   1
+1   1   2
+1   1   2   2
+
+1       2
+1       2   2
+1           2   duplicate skip the second 2
+
+    1           duplicate skip the second 1
+    1   2       duplicate skip the second 1
+    1   2   2   duplicate skip the second 1
+
 
 
 """
